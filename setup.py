8d3f4861f3da3acd56716687bfa06175cfb4f107
Jon Coppeard <jon@coppeard.net>
2012-01-29 18:52:14 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = 'Varas',
+      version = '0.1',
+      description = 'Pratt parser framework',
+      author = 'Jon Coppeard',
+      author_email = 'jon@coppeard.net',
+      py_modules = [ 'src/varas' ],
+     )
